3dc2a50d7923362f0fa80c3148f4f321632d5251
Suneel Kumar <shrikantsinghkashmeawahi@gmail.com>
2024-05-29 23:11:11 +0530

    add class work _1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="wrapper">
+         <h1>Learn web development</h1>
+          <div className="para">
+          <p>Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites</p>
+          </div>
+          <div className="para">
+          <p>The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/" target="_blank">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+          </div>
+          <div className="para">
+          <p>If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
+          </div>
+         
+        
+        
+      </div>
+     </>
+  )
+}
+
+export default App
